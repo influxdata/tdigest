d5faa526e8f0a219fd17f8d95bf41ff7bfe722ff
Ilya Shcherbak <i.shcherbak@corp.vk.com>
2019-11-22 13:45:37 +0300

    Fixed tdigest.Centroidlist test issue.

--- tdigest_test.go
+++ tdigest_test.go
@@ -336,9 +336,9 @@ func TestTdigest_Centroids(t *testing.T) {
 		},
 	}
 
-	var got tdigest.CentroidList
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			var got tdigest.CentroidList
 			td := tt.digest
 			if td == nil {
 				td = tdigest.NewWithCompression(3)
